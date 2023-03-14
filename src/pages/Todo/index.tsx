0cd92ed0a0b02d5a65705d6d794cf04cae7bae72
Karoline <kquetz@viceri.com.br>
2023-03-14 17:56:59 -0300

    Feito componente para ser exibido caso não tenha nenhuma tarefa

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,13 +1,21 @@
 import { Header } from "../../components/Header";
 import { NewToDo } from "../../components/NewToDo";
+import clipboard from "../../assets/clipboard.svg";
+
 import {
+  ClipboardImg,
   Completed,
   CountContainer,
+  DeleteTask,
+  ListItem,
+  NoTasks,
   TasksCreated,
   ToDoContainer,
 } from "./styles";
+import { Check, Trash } from "phosphor-react";
 
 export const Todo = () => {
+  const value = true;
   return (
     <>
       <Header />
@@ -22,6 +30,26 @@ export const Todo = () => {
             Concluídas: <span>2 de 5</span>
           </Completed>
         </CountContainer>
+
+        {value ? (
+          <ListItem>
+            <Check size={20} />
+            <span>
+              Integer urna interdum massa libero auctor neque turpis turpis
+              semper. Duis vel sed fames integer.
+            </span>
+
+            <DeleteTask>
+              <Trash size={20} />
+            </DeleteTask>
+          </ListItem>
+        ) : (
+          <NoTasks>
+            <ClipboardImg src={clipboard} alt="" />
+            <strong> Você ainda não tem tarefas cadastradas</strong>
+            Crie tarefas e organize seus itens a fazer
+          </NoTasks>
+        )}
       </ToDoContainer>
     </>
   );
--- src/pages/Todo/styles.ts
+++ src/pages/Todo/styles.ts
@@ -36,3 +36,52 @@ export const Completed = styled.div`
     padding: 0.125rem 0.5rem;
   }
 `;
+
+export const NoTasks = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  margin-top: 1.5rem;
+  padding: 4rem 1.5rem;
+
+  border-top: 1px solid;
+  border-radius: 8px;
+`;
+
+export const ClipboardImg = styled.img`
+  padding: 1rem;
+`;
+
+export const ListItem = styled.div`
+  height: 4.5rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  background: ${(props) => props.theme["gray 400"]};
+  color: ${(props) => props.theme["gray 100"]};
+
+  border-radius: 8px;
+
+  span {
+    max-width: 39.5rem;
+  }
+`;
+
+export const DeleteTask = styled.button`
+  background: transparent;
+  cursor: pointer;
+  border: 0;
+  line-height: 0;
+  color: ${(props) => props.theme["gray 300"]};
+
+  &:hover {
+    background: ${(props) => props.theme["gray 400"]};
+    color: ${(props) => props.theme.danger};
+    border-radius: 2px;
+  }
+`;
